faec424e7b92988c5f87a59ca47112b7546effbb
slawless08 <samuel.l.lawless@gmail.com>
2021-07-14 18:37:37 -0400

    Created package.json. Added inital employee constructor and functions in the index.js.

---
+++ index.js
@@ -0,0 +1,27 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = empName;
+        this.id = empId; 
+        this.email = empEmail;
+    }
+
+    getName(){
+        console.log(empName);
+        return empName
+    }
+
+    getId(){
+        console.log(empId);
+        return empId
+    }
+
+    getEmail(){
+        console.log(empEmail);
+        return empEmail
+    }
+
+    getRole(){
+        console.log("Employee");
+        return "Employee"
+    }
+}
